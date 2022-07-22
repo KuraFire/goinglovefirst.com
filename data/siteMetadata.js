150d2c931d73f450025cef6334983a8831a45113
Faruk Ates <farukates@me.com>
2022-07-22 16:37:29 -0700

    facepalm emoji

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/kurafire/goinglovefirst.com',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.jpeg',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/open-graph.png',
   email: '',
   // github: 'https://github.com',
   twitter: 'https://twitter.com/KuraFire',
