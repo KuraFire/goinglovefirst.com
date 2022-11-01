34c12fc27247cbcf391a155cd44db64c80a4d244
Faruk Ates <farukates@me.com>
2022-10-31 18:25:05 -0700

    Return of the Vault

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -36,40 +36,7 @@ const NewsletterForm = ({ title = 'The Newsletter' }) => {
 
   return (
     <div className="mt-2 block">
-      <p className="text-xl font-semibold text-gray-900">{title}</p>
-      {/* <p className="my-3 text-base text-gray-500">Receive the latest updates in your inbox.</p> */}
-      {/* <form className="mt-3 flex flex-col" onSubmit={subscribe}>
-        <div>
-          <label className="sr-only" htmlFor="email-input">
-            Email address
-          </label>
-          <input
-            autoComplete="email"
-            className="w-full rounded-md px-4 placeholder:text-slate-400 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-pink-300"
-            id="email-input"
-            name="email"
-            placeholder={subscribed ? 'Confirm via your inbox!  🎉' : 'Enter your email'}
-            ref={inputEl}
-            required
-            type="email"
-            disabled={subscribed}
-          />
-        </div>
-        <div className="mt-2 flex w-full justify-end">
-          <button
-            className={`inline-block rounded-md bg-pink-400 py-2 px-4 font-medium text-white shadow-sm shadow-pink-400/21 ${
-              subscribed ? 'cursor-default' : 'hover:bg-pink-700'
-            } focus:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2`}
-            type="submit"
-            disabled={subscribed}
-          >
-            {subscribed ? 'Thank you!' : 'Sign up ♥'}
-          </button>
-        </div>
-      </form>
-      {error && (
-        <div className="w-72 pt-2 text-sm text-red-500 dark:text-red-400 sm:w-96">{message}</div>
-      )} */}
+      <h2 className="mb-6 text-xl font-semibold text-gray-900">{title}</h2>
       <div id="revue-embed">
         <form
           // action="https://newsletter.goinglovefirst.com/add_subscriber"
@@ -79,76 +46,48 @@ const NewsletterForm = ({ title = 'The Newsletter' }) => {
           // target="_blank"
           onSubmit={subscribe}
         >
-          <div className="mt-4 mb-2 flex -space-x-px text-small">
-            <div className="revue-form-group relative w-1/2">
-              <input
-                className="peer w-full rounded-l-md border-gray-400 text-small placeholder-transparent transition-all"
-                // placeholder="Email address…"
-                placeholder={subscribed ? 'You’re subscribed!  🎉' : 'Email (required)'}
-                type="email"
-                // getRevue's own form HTML:
-                // name="member[email]"
-                // id="member_email"
-                id="email-input"
-                name="email"
-                // Tailwind starter code:
-                ref={inputEl}
-                disabled={subscribed}
-                required
-              />
-              <label
-                htmlFor="member_email"
-                className="absolute left-2 -top-2.5 whitespace-nowrap bg-white px-1 text-xs text-gray-600 transition-all peer-placeholder-shown:top-2.5 peer-focus:-top-2.5 peer-focus:text-blue-600"
-              >
-                Email ({' '}
-                <span title="Required field" className="required -ml-0.5 text-red-300">
+          <div className="mt-6 mb-4 flex text-small">
+            <div className="revue-form-group relative w-full">
+              <div className="relative focus-within:z-10">
+                <input
+                  className="peer w-full rounded-l-md border-gray-400 border-r-pink-500 text-small placeholder-transparent shadow-sm shadow-gray-400/20 transition-all focus:border-pink-500 focus:ring-pink-500"
+                  // placeholder="Email address…"
+                  placeholder={subscribed ? 'You’re subscribed!  🎉' : 'Email (required)'}
+                  type="email"
+                  // getRevue's own form HTML:
+                  // name="member[email]"
+                  // id="member_email"
+                  id="email-input"
+                  name="email"
+                  // Tailwind starter code:
+                  ref={inputEl}
+                  disabled={subscribed}
                   required
-                </span>{' '}
-                )
-              </label>
+                />
+                <label
+                  htmlFor="member_email"
+                  className="absolute left-2 -top-2.5 whitespace-nowrap bg-white px-1 text-xs text-gray-600 transition-all peer-placeholder-shown:top-2.5 peer-focus:-top-2.5 peer-focus:text-pink-600"
+                >
+                  Email ({' '}
+                  <span title="Required field" className="required -ml-0.5 text-red-300">
+                    required
+                  </span>{' '}
+                  )
+                </label>
+              </div>
             </div>
-            <div className="revue-form-group relative w-1/4">
-              <input
-                className="peer w-full border-gray-400 text-small placeholder-transparent transition-all"
-                placeholder="First name…"
-                type="text"
-                name="member[first_name]"
-                id="member_first_name"
-              />
-              <label
-                htmlFor="member_first_name"
-                className="absolute left-2 -top-2.5 whitespace-nowrap bg-white px-1 text-xs text-gray-600 transition-all peer-placeholder-shown:top-2.5 peer-focus:-top-2.5 peer-focus:text-blue-600"
-              >
-                First name <span className="optional sr-only">(Optional)</span>
-              </label>
-            </div>
-            <div className="revue-form-group relative w-1/4">
-              <input
-                className="peer w-full rounded-r-md border-gray-400 text-small placeholder-transparent transition-all"
-                placeholder="Last…"
-                type="text"
-                name="member[last_name]"
-                id="member_last_name"
-              />
-              <label
-                htmlFor="member_last_name"
-                className="absolute left-2 -top-2.5 whitespace-nowrap bg-white px-1 text-xs text-gray-600 transition-all peer-placeholder-shown:top-2.5 peer-focus:-top-2.5 peer-focus:text-blue-600"
-              >
-                Last name <span className="optional sr-only">(Optional)</span>
-              </label>
-            </div>
-          </div>
-          <div
-            className="revue-form-actions flex flex-row-reverse items-center"
-            title="By subscribing, you agree with Revue’s Terms of Service and Privacy Policy."
-          >
             <input
               type="submit"
               value={subscribed ? 'Thank you!' : 'Subscribe ♥'}
               name="member[subscribe]"
               id="member_submit"
-              className="inline-block rounded-md bg-pink-400 py-1.5 px-4 font-medium text-white shadow-sm shadow-pink-400/21 hover:bg-pink-700 focus:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+              className="relative inline-block rounded-l-none rounded-r-md bg-pink-400 py-1.5 px-4 font-medium text-white shadow-sm shadow-pink-400/21 transition-colors hover:bg-pink-300 focus:z-10 focus:bg-pink-50 focus:text-pink-700 focus:ring-pink-500"
             />
+          </div>
+          <div
+            className="revue-form-actions flex flex-row-reverse items-center"
+            title="By subscribing, you agree with Revue’s Terms of Service and Privacy Policy."
+          >
             <div className="revue-form-footer grow space-x-1 text-xs text-gray-500">
               <a target="_blank" href="https://www.getrevue.co/terms" rel="noreferrer">
                 Terms of Service
--- components/SubscribeBoxes.js
+++ components/SubscribeBoxes.js
@@ -3,11 +3,15 @@ import Image from 'next/image'
 
 export default function SubscribeBoxes({ children }) {
   return (
-    <div className="mx-1 mt-12 -mb-36 grid gap-8 md:mx-3 md:grid-cols-2">
+    <div className="mx-1 mt-12 -mb-36 grid gap-8 md:-mx-10 md:grid-cols-4 lg:-mx-32 xl:-mx-[9rem] xl:grid-cols-6 2xl:-mx-55">
+      {/* 
+        ORIGINAL for two:
+        <div className="mx-1 mt-12 -mb-36 grid gap-8 md:mx-3 md:grid-cols-2"> 
+      */}
       {/* <div className="mx-auto mt-12 -mb-36 grid max-w-lg gap-8 lg:-mx-24 lg:max-w-none lg:grid-cols-3"> */}
       {/* The Newsletter */}
-      <div className="group flex flex-col overflow-hidden rounded-lg shadow-lg transition duration-200 focus-within:shadow-xl hover:shadow-xl">
-        <picture className="flex-shrink-0">
+      <div className="group col-span-2 flex flex-col overflow-hidden rounded-lg bg-white shadow-lg transition duration-200 focus-within:shadow-xl hover:shadow-xl">
+        <picture className="shrink-0">
           {/* <img className="h-48 w-full object-cover" src="/static/images/heart-mail.jpeg" alt="" /> */}
           <Image
             className="h-48 w-full object-cover transition group-focus-within:scale-110 group-hover:scale-110"
@@ -19,7 +23,11 @@ export default function SubscribeBoxes({ children }) {
         </picture>
         <div className="flex flex-1 flex-col justify-between bg-white p-6">
           <div className="flex-1">
-            <p className="text-sm font-medium text-slate-400">your inbox</p>
+            <p className="text-sm font-medium text-slate-400">
+              <a href="https://newsletter.goinglovefirst.com/" className="hover:underline">
+                newsletter.goinglovefirst.com
+              </a>
+            </p>
             <NewsletterForm />
           </div>
         </div>
@@ -28,12 +36,12 @@ export default function SubscribeBoxes({ children }) {
       {/* The Publication */}
       <a
         href="https://medium.com/lovefirst"
-        className="group flex flex-col overflow-hidden rounded-lg shadow-lg transition duration-100 hover:shadow-xl focus:shadow-xl"
+        className="group col-span-2 flex flex-col overflow-hidden rounded-lg bg-white shadow-lg transition duration-100 hover:shadow-xl focus:shadow-xl"
       >
-        <picture className="flex-shrink-0">
+        <picture className="shrink-0">
           {/* <img className="h-48 w-full object-cover" src="/static/images/love-is-tree.jpeg" alt="" /> */}
           <Image
-            className="h-48 w-full origin-bottom object-cover transition group-hover:scale-110"
+            className="h-48 origin-bottom object-cover transition group-hover:scale-110"
             width="2100"
             height="1181"
             src="/static/images/love-is-tree.jpeg"
@@ -46,7 +54,7 @@ export default function SubscribeBoxes({ children }) {
               medium.com/lovefirst
             </p>
             <div className="mt-2 block">
-              <p className="text-xl font-semibold text-gray-900">The Publication</p>
+              <h2 className="mb-5 text-xl font-semibold text-gray-900">The Publication</h2>
               <p className="mt-3 text-base text-gray-500">
                 Join thousands of readers on Medium and participate in the conversation.
               </p>
@@ -56,26 +64,34 @@ export default function SubscribeBoxes({ children }) {
       </a>
 
       {/* The Vault */}
-      {/* <a
-      href="https://vault.goinglovefirst.com"
-      className="flex  flex-col overflow-hidden rounded-lg shadow-lg transition duration-100 hover:shadow-xl focus:shadow-xl"
-    >
-      <picture className="flex-shrink-0">
-        <img className="h-48 w-full object-cover" src="/static/images/open-vault.jpeg" alt="" />
-      </picture>
-      <div className="flex flex-1 flex-col justify-between bg-white p-6">
-        <div className="flex-1">
-          <p className="text-sm font-medium text-slate-400">vault.goinglovefirst.com</p>
-          <div className="mt-2 block">
-            <p className="text-xl font-semibold text-gray-900">The Vault</p>
-            <p className="mt-3 text-base text-gray-500">
-              Incomplete pieces of the Love First Puzzle are stored in this Obsidian-powered
-              repository.
+      <a
+        href="https://vault.goinglovefirst.com"
+        className="group col-span-2 flex flex-col overflow-hidden rounded-lg bg-white shadow-lg transition duration-100 hover:shadow-xl focus:shadow-xl md:col-start-2 xl:col-start-5"
+      >
+        <picture className="shrink-0">
+          <Image
+            className="h-48 w-full object-cover transition group-hover:scale-110"
+            src="/static/images/open-vault.jpeg"
+            width="2100"
+            height="1181"
+            alt=""
+          />
+        </picture>
+        <div className="flex flex-1 flex-col justify-between bg-white p-6">
+          <div className="flex-1">
+            <p className="text-sm font-medium text-slate-400 group-hover:underline group-focus:underline">
+              vault.goinglovefirst.com
             </p>
+            <div className="mt-2 block">
+              <p className="text-xl font-semibold text-gray-900">The Vault</p>
+              <p className="mt-3 text-base text-gray-500">
+                Incomplete pieces of the Love First Puzzle are stored in this Obsidian-powered
+                repository.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </a> */}
+      </a>
     </div>
   )
 }
